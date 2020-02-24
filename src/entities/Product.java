707609f15c954bd399e4d7dd3af19b52b12ca5db
leandrole8 <leandro_le8@hotmail.com>
2020-02-24 11:16:48 -0300

    Criação do construtor, getters e setters, metodos ValorTotalEmEstoque, AddProduto e RemoverProduto

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -6,6 +6,46 @@ public class Product {
 	private double preco;
 	private int quantidade;
 	
-	
+	public Product(String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public double ValorTotalEmEstoque(double total) {
+		return total = quantidade*preco;
+	}
+	
+	public void AddProduto(int quantidade) {
+		quantidade = getQuantidade() + quantidade;
+	}
+	
+	public void RemoverProduto(int quantidade) {
+		quantidade = getQuantidade() - quantidade;
+	}
+	
 }
